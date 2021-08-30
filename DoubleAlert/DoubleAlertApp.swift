8f278b837c347464397373f9abd717e8fb6c525c
Yuri Goncharov <yurig@qualtie.com>
2021-08-30 18:09:23 +0300

    Initial Commit

---
+++ DoubleAlert/DoubleAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleAlertApp.swift
+//  DoubleAlert
+//
+//  Created by Yuri Goncharov on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct DoubleAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
